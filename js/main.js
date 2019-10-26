2ac46817e46778b4808763a5e1669c47fd383bfd
Michael Garnreiter <mgarnreiter@aol.com>
2019-10-26 19:09:33 +0200

    inital project setup with p5.js

---
+++ js/main.js
@@ -0,0 +1,15 @@
+console.log("check");
+
+function preload() {
+  console.log("preload");
+}
+
+function setup() {
+  console.log("setup");
+  createCanvas(1200, 800);
+}
+
+function draw() {
+  console.log("draw");
+  background("green");
+}
